275673be54a5973847df3540e1e882772c87a891
Eric Gao <ejgao@ucdavis.edu>
2024-07-23 17:35:24 -0500

    changed file

--- tgc/data/__init__.py
+++ tgc/data/__init__.py
@@ -1,6 +1,6 @@
 from .lorenz import lorenz_96
 from .var import var_stable
-from .var import custom_var_function
+#from .var import custom_var_function
 from .fMRI import fmri_net_sim
 from .dream3 import dream3_trajectories
 from .dream4 import dream4_trajectories
